52e8b12e4e2c524adde3cf5b70d237a1c90014dd
augtal <augustas.talacka@gmail.com>
2020-08-19 19:36:14 +0300

    Added excel sheet analysis

--- app.py
+++ app.py
@@ -3,10 +3,33 @@ from openpyxl.chart import BarChart, Reference
 
 
 def process_workbook(filename):
-    wb = xl.load_workbook(filename)
+    workbook = xl.load_workbook(filename)
+    finalSheet = workbook.create_sheet(title="Final")
 
-    sheet = wb['Sheet1']
+    for sheet in workbook:
+        for row in sheet.iter_rows(min_row=1, max_col=sheet.max_column, max_row=10):
+            if row[0].value is None:
+                continue
+            elif row[0].value.find("Ex"):
+                continue
+            else:
+                for cell in row:
+                    if cell.value is None:
+                        continue
+                    else:
+                        print(cell.value)
 
+    filename += "5"
+    # workbook.save(filename)
+
+    print('Done')
+
+
+file_name = 'Test1.xlsx'
+process_workbook(file_name)
+
+
+"""
     for row in range(2, sheet.max_row+1):
         cell = sheet.cell(row, 3)
         corrected_price = cell.value * 0.9
@@ -20,10 +43,14 @@ def process_workbook(filename):
     chart = BarChart()
     chart.add_data(values)
     sheet.add_chart(chart, 'e2')
+"""
 
-    wb.save(filename)
+"""
+        for row in range(1, sheet.max_row+1):
+            cell = sheet.cell(row,1)
+            if cell.value is None:
+                continue
 
-    print('Done')
-
-
-process_workbook('transactions.xlsx')
+            if not cell.value.find("Ex"):
+                sheet.unmerge_cells(start_row=row, start_column=1, end_row=row, end_column=sheet.max_column)
+"""
