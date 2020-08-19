76299ecf69d76e71970c58dcdfd2fa74a70bbaf7
augtal <augustas.talacka@gmail.com>
2020-08-19 14:14:52 +0300

    Reading spreadsheet values

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import openpyxl as xl
 
-print("Hi")
+wb = xl.load_workbook('transactions.xlsx')
+
+sheet = wb['Sheet1']
+
+for row in range(2, sheet.max_row+1):
+    print(row)
+    cell = sheet.cell(row,3)
+    print(cell.value)
+
+print('Done')
